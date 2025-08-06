88acea58b0c28ca5f95a25c21bc04873d63267ff
Amanda J Kendal-Brown <15990675+ajkendal@users.noreply.github.com>
2025-08-06 12:49:57 -0700

    adding boost component

---
+++ src/components/Boost.tsx
@@ -0,0 +1,13 @@
+import '../styles/boost.scss';
+
+const Boost = () => {
+  return (
+    <div className='boost-div'>
+      <h2 className='boost-header'>Boost your links today</h2>
+
+      <button className='radius'>Get Started</button>
+    </div>
+  );
+};
+
+export default Boost;
